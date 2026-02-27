4106dc1e08ffeb33132f7897588a094c244b97dd
Thomas Lutsky <tlutsky@csilvm-16.cs.ucsb.edu>
2026-02-27 15:27:15 -0800

    Added Comments

--- lab08/ll_functions.cpp
+++ lab08/ll_functions.cpp
@@ -5,19 +5,21 @@
 // This function is started for you correctly - you have to complete it.
 //   Use the comments to help you figure out what to do.
 void h_insert(LinkNodePtr& head, string nom, int num) {
-    LinkNodePtr tmp_ptr;
-    tmp_ptr = new LinkNode;
+    //Preconditions: Takes in the head pointer passed by reference, a string, and an integer. The string and integer will be the values associated with the new Node
+    //Postconditions: Adds a new node to the linked list with the associated values
+    LinkNodePtr tmp_ptr; //pointer creation
+    tmp_ptr = new LinkNode; //Temporary node creation
 
-    tmp_ptr->name = nom;
-    tmp_ptr->number = num;
-    tmp_ptr->link = nullptr; 
+    tmp_ptr->name = nom; //the new node's name is now the string nom
+    tmp_ptr->number = num; //the new node's number is now the integer num
+    tmp_ptr->link = nullptr;  //set the link of the node to NULL as it will be inserted at the end of the linked list
     
     LinkNodePtr traversal_ptr; //create a pointer to traverse the list to get to the last node
    if(head == NULL){ //if list is empty, set the head pointer to the node tmp_ptr points to
         head = tmp_ptr;
     }
     else{
-        traversal_ptr = head; 
+        traversal_ptr = head; //traversal pointer set to the same value as the head pointer
         while(traversal_ptr->link != NULL){ //gets to the last node
             traversal_ptr = traversal_ptr->link; 
         }
@@ -26,123 +28,132 @@ void h_insert(LinkNodePtr& head, string nom, int num) {
 }
 
 void createLL(LinkNodePtr& h){
-    // Incomplete function - student must complete
+    //Preconditions: Takes in the head pointer by reference
+    //Postconditions: inserts a node to the end of the linked list evey time the user gives a new set of info, stop when user types: 0 0
     string nom = "";
     int num;
     while(nom != "0" || num != 0){
-        cout << "Enter name, then a number. To quit, enter 0 for the name AND 0 for the number:\n";
-        cin >> nom >> num;
-        if(nom != "0" || num != 0){
+        cout << "Enter name, then a number. To quit, enter 0 for the name AND 0 for the number:\n"; //user prompt to get name and number
+        cin >> nom >> num; //user input
+        if(nom != "0" || num != 0){ //if the user decided not to exit, insert the node with the given information
             h_insert(h, nom, num);
         }
     }
 }
 
 void printLL(LinkNodePtr h){
-    LinkNodePtr traversalPtr = nullptr;
-    if(h == NULL){
+    //Preconditions: The head pointer
+    //Postconditions: Prints out the linked list
+    LinkNodePtr traversalPtr = nullptr; //pointer created to traverse nodes
+    if(h == NULL){ //if the list is empty (h points to nothing)
         cout << "This list is empty\n";
         return;
     }
-    else{
-        cout << "Printing the list:\n";
-        traversalPtr = h;
+    else{ //the list has entries
+        cout << "Printing the list:\n"; 
+        traversalPtr = h; //traversal pointer points to the same node h points to
         int count(0);
-        while(traversalPtr != NULL){
-            cout << "Node #" << count << ": ";
-            cout << traversalPtr->name << ", ";
-            cout << traversalPtr->number << "\n";
-            traversalPtr = traversalPtr->link;
-            count++;
+        while(traversalPtr != NULL){ //if the pointer no longer points to a node, then stop the loop
+            cout << "Node #" << count << ": "; //giving the node position
+            cout << traversalPtr->name << ", "; //outputs name within node
+            cout << traversalPtr->number << "\n"; //outputs number within node
+            traversalPtr = traversalPtr->link; //point to the next node
+            count++; //increment the count as one node has been added to our knowledge
         }
     }
 }
 
 void insertNodeAfter(LinkNodePtr& h) {
-    if(h == nullptr){
+    //Preconditions: Takes in the head pointer by reference
+    //Postconditions: Inserts a node with given information after the specified position
+    if(h == nullptr){ //if the list is empty (h points to nothing)
         cout << "Cannot insert in a non-existing link\n";
         cout << "This list is empty\n";
         return;
     }
     int nodeAfter;
-    LinkNodePtr traversalPtr = nullptr;
-    cout << "Enter node position to insert new node after (enter negative number to exit): ";
-    cin >> nodeAfter;
-    if(nodeAfter < 0){
+    LinkNodePtr traversalPtr = nullptr; //pointer created to traverse linked list
+    cout << "Enter node position to insert new node after (enter negative number to exit): "; //user prompt
+    cin >> nodeAfter; //user input
+    if(nodeAfter < 0){ //if the user chose to exit (entered a negative number)
         return;
     }
     else{
-        traversalPtr = h;
+        traversalPtr = h; //traversalPtr now points to the first node
         int count(0);
-        while(traversalPtr != NULL){
-            count++;
-            traversalPtr = traversalPtr->link;
+        while(traversalPtr != NULL){ //loop continues unless traversalPtr does not point to a node
+            count++; //increment the count to get the amount of nodes
+            traversalPtr = traversalPtr->link; //point to the next node
         }
-        if(nodeAfter >= count){
+        if(nodeAfter >= count){ //if the user inputs a position that is out of the node range then give an error
             cout << "Position entered is illegal. Nothing inserted\n";
             return;
         }
         string nom = "";
         int num;
-        cout << "Enter data (name, then number): ";
-        cin >> nom >> num;
-        traversalPtr = h;
-        LinkNodePtr tempPtr = nullptr;
-        tempPtr = new LinkNode;
-        tempPtr->name = nom;
-        tempPtr->number = num;
-        for(int i = 0; i < nodeAfter; i++){
-            if(traversalPtr == nullptr){
+        cout << "Enter data (name, then number): "; //user prompt
+        cin >> nom >> num; //user input
+        traversalPtr = h; //traversalPtr now points to first node
+        LinkNodePtr tempPtr = nullptr; //create a pointer to point to the node with the information to add
+        tempPtr = new LinkNode; //new node created
+        tempPtr->name = nom; //data insertion
+        tempPtr->number = num; //data insertion
+        for(int i = 0; i < nodeAfter; i++){ //gets to the node you want to insert after
+            if(traversalPtr == nullptr){ //for any reason, if the traversalPtr becomes NULL, then it gives an error (should already be handled by a previous error)
                 cout << "Cannot insert in a non-existing link\n";
                 return;
             }
-            traversalPtr = traversalPtr->link;
+            traversalPtr = traversalPtr->link; //points to next node
         }
-        if(traversalPtr == nullptr){
+        if(traversalPtr == nullptr){ //again if traversalPtr no longer points to anything, gives an error
             cout << "Cannot insert in a non-existing link\n";
             return;
         }
-        tempPtr->link = traversalPtr->link;
-        traversalPtr->link = tempPtr;
+        tempPtr->link = traversalPtr->link; //the inserted node's link is now the node that nodeAfter was pointing to
+        traversalPtr->link = tempPtr; //nodeAfter now points to inserted node
     }        
 }
 
 void findMax(LinkNodePtr h) {
+    //Preconditions: Takes in the pointer to the linked list
+    //Postconditions: Finds the maximum value for number among all the nodes within the linked list
     int max(0), current;
-    LinkNodePtr traversalPtr;
+    LinkNodePtr traversalPtr; //pointer created to traverse linked list
     traversalPtr = h;
-    if(h == nullptr){
+    if(h == nullptr){ //if the list is empty (h points to nothing), return
         cout << "This list is empty\n";
         return;
     }
-    while(traversalPtr != NULL){
-        current = traversalPtr->number;
-        if(max < current){
+    max = traversalPtr->number; //this sets the maximum to the first number in the linked list (if there is only one value it is the maximum)
+    while(traversalPtr != NULL){ //exits loop if traversalPtr no longer points to a node
+        current = traversalPtr->number; //the current value is assigned to the value within the given node
+        if(max < current){ //if the "max" is less than the current value, set max to that value
             max = current;
         }
-        traversalPtr = traversalPtr->link;
+        traversalPtr = traversalPtr->link; //move onto the next node
     }
-    cout << "Largest number in the list is: " << max << endl;
+    cout << "Largest number in the list is: " << max << endl; //output
 }
 
 void findMin(LinkNodePtr h) {
-    // Incomplete function - student must complete
+    //Preconditions: Takes in the pointer to the linked list
+    //Postconditions: Find the minimum value for number among all the nodes within the linked list
     int min(0), current;
-    LinkNodePtr traversalPtr;
-    traversalPtr = h;
-    if(h == nullptr){
+    LinkNodePtr traversalPtr; //pointer creation to traverse linked list
+    traversalPtr = h; //traversalPtr now points to start of linked list
+    if(h == nullptr){ //if h points to nothing then the list is empty
         cout << "This list is empty\n";
         return;
     }
-    min = traversalPtr->number;
-    while(traversalPtr != NULL){
-        current = traversalPtr->number;
-        if(min > current){
+    min = traversalPtr->number; //this sets the minimum to the first number in the linked list
+    while(traversalPtr != NULL){ //exits loop if traversalPtr no longer points to a node
+        current = traversalPtr->number; //the current value is assigned to the value within the given node
+        if(min > current){ //if "min" is less than the current value, set min to that value
             min = current;
         }
-        traversalPtr = traversalPtr->link;
+        traversalPtr = traversalPtr->link; //move onto the next node
     }
-    cout << "Smallest number in the list is: " << min << endl;
+    cout << "Smallest number in the list is: " << min << endl; //output
 }
 
 
--- lab08/r_functions.cpp
+++ lab08/r_functions.cpp
@@ -6,7 +6,7 @@ const string OUTPUTFILE = "Results.txt";
 void NameSort(UndergradStudents array[], int size){
     //Preconditions: Takes in an array of type UndergradStudents and the size of that array
     //Postconditions: Sorts elements of the array according to the alphabetical order of the last member function's variable within each element
-    for(int i = size - 1; i >= 0; i--){
+    for(int i = size - 1; i >= 0; i--){ //the bubble sort syntax but instead if the first char of the last name is further down the alphabet, then switch
         for(int j = 1; j <= i; j++){
             string temp1 = array[j-1].last;
             string temp2 = array[j].last;
@@ -19,62 +19,66 @@ void NameSort(UndergradStudents array[], int size){
     }
 }
 void InitializeStructures(UndergradStudents us[], int &size){
+    //Preconditions: Takes in an array of type UndergradStudents and the size of the array by reference
+    //Postconditions: Uses user input to add values for each student that the user has
     int count(0);
     int id_num(0);
     cout << showpoint;
-    cout << setprecision(2);
-    for(int i = 0; i < 20; i++){
+    cout << setprecision(2); //set to 2 decimal places
+    for(int i = 0; i < 20; i++){ //loop to get all the entries from the user (20 maximum entries)
         string first;
-        if(i != 0){
+        if(i != 0){ //formatting
             cout << endl;
         }
-        cout << "Student 0" << count + 1 << ":Enter first name (or x to quit): ";
+        cout << "Student 0" << count + 1 << ":Enter first name (or x to quit): "; //user prompt with the respective student number (starts at Student 01)
         cin >> first;
-        if(first == "x" || first == "X"){
+        if(first == "x" || first == "X"){ //if the user chooses to quit, then exit the for loop
             break;
         }
-        count++;
-        id_num++;
-        us[i].first = first;
-        string last, major;
+        count++; //increment the count as this is the next student 
+        id_num++; //increment the id_num for the respective student
+        us[i].first = first; //the structure at the ith index now has the user entered first name
+        string last, major; 
         float GPA1, GPA2, GPA3, GPA4;
-        cout << "Student 0" << count << ":Enter last name: ";
+        cout << "Student 0" << count << ":Enter last name: "; //prompt to get last name
         cin >> last;
-        cout << "Student 0" << count << ":Enter major: ";
+        cout << "Student 0" << count << ":Enter major: "; //prompt to get major
         cin >> major;
-        cout << "Student 0" << count << ":Enter GPA Year 1: ";
+        cout << "Student 0" << count << ":Enter GPA Year 1: "; //prompt to get 1st GPA
         cin >> GPA1;
-        cout << "Student 0" << count << ":Enter GPA Year 2: ";
+        cout << "Student 0" << count << ":Enter GPA Year 2: "; //prompt to get 2nd GPA
         cin >> GPA2;
-        cout << "Student 0" << count << ":Enter GPA Year 3: ";
+        cout << "Student 0" << count << ":Enter GPA Year 3: "; //prompt to get 3rd GPA
         cin >> GPA3;
-        cout << "Student 0" << count << ":Enter GPA Year 4: ";
+        cout << "Student 0" << count << ":Enter GPA Year 4: "; //prompt to get 4th GPA
         cin >> GPA4;
-        us[i].last = last;
-        us[i].major = major;
-        double average = (GPA1 + GPA2 + GPA3 + GPA4) / 4;
-        us[i].GPA_avg = average;
-        us[i].ID_num = id_num;
+        us[i].last = last; //add the last name to the structure at the ith index
+        us[i].major = major; //add the major to the structure at the ith index
+        double average = (GPA1 + GPA2 + GPA3 + GPA4) / 4; //average GPA among the four years
+        us[i].GPA_avg = average; //add the average GPA to the structure at the ith index
+        us[i].ID_num = id_num; //add the student ID to the structure at the ith index
     }
-    size = count;
+    size = count; //change the size of the array to the count of structures made within the for loop (maximum 20)
 }
    
 void WriteResults(ofstream &outf, UndergradStudents us[], int size){
-    outf.open(OUTPUTFILE);
-    if(outf.fail()){
+    //Preconditions: Takes in the name of the outfile, the array of structures(student information), and the size of the array
+    //Postconditions: Prints out the respective information for each student
+    outf.open(OUTPUTFILE); //open the outFile
+    if(outf.fail()){ //if the output File fails to open, send an error
         cerr << "Failed to open output file";
         exit(1);
     }
-    outf << "These are the results sorted by last name:\n";
-    NameSort(us, size);
-    for(int i = 0; i < size; i++){
-        outf << "ID #" << us[i].ID_num << ": ";
-        outf << us[i].last << ": ";
-        outf << us[i].first << ": ";
-        outf << us[i].major << ": ";
-        outf << us[i].GPA_avg << endl;
+    outf << "These are the results sorted by last name:\n"; 
+    NameSort(us, size); //sorts the structures by the alphabetical order of last names
+    for(int i = 0; i < size; i++){ //gives the number of outputs for the number of students
+        outf << "ID #" << us[i].ID_num << ": "; //outputs student ID
+        outf << us[i].last << ": "; //outputs student last name
+        outf << us[i].first << ": "; //outputs student first name
+        outf << us[i].major << ": "; //outputs student major
+        outf << us[i].GPA_avg << endl; //outputs student average GPA
     }
-    outf.close();
+    outf.close(); //close connection to the out file
     return;
 }
 
