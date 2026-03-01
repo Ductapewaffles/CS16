c7b53ffda509926fb3c7a398377101c9d0d909a1
Thomas Lutsky <tlutsky@csilvm-16.cs.ucsb.edu>
2026-03-01 15:54:18 -0800

    Changed name to r_structs.cpp

---
+++ lab08/r_structs.cpp
@@ -0,0 +1,11 @@
+// file: r_structs.h
+// Thomas Lutsky
+
+// DECLARE THE struct UndergradStudents HERE:
+struct UndergradStudents{
+    int ID_num;
+    string first;
+    string last;
+    string major;
+    double GPA_avg;
+};
